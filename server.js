2a62ef1704a66dbbda8184010918dd9234633080
Elad Hirsch <eladh@jfrog.com>
2019-09-20 14:02:17 +0300

    fix app

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
+const http = require('http');
+const fileSystem = require('fs');
 
-var http = require('http');
-var fileSystem = require('fs');
 function msleep(n) {
   Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
 }
 
-var server = http.createServer(function(req, resp){
-	fileSystem.readFile('./index.html', function(error, fileContent){
-		if(error){
-			resp.writeHead(500, {'Content-Type': 'text/plain'});
-			resp.end('Error');
-		}
-		else{
-			resp.writeHead(200, {'Content-Type': 'text/html'});
-			resp.write(fileContent);
-			resp.end();
-		}
-	});
+const server = http.createServer(function (req, resp) {
+	msleep(3 * 1000);
+    fileSystem.readFile('./index.html', function (error, fileContent) {
+        if (error) {
+            resp.writeHead(500, {'Content-Type': 'text/plain'});
+            resp.end('Error');
+        }
+        else {
+            resp.writeHead(200, {'Content-Type': 'text/html'});
+            resp.write(fileContent);
+            resp.end();
+        }
+    });
 });
 
 server.listen(8080);
